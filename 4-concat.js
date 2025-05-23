30779dfdb457030a976ccff8d714f135461c33e0
kariukik <kariukioh@gmail.com>
2025-05-23 19:23:32 +0300

    argument location access

--- 4-concat.js
+++ 4-concat.js
@@ -2,6 +2,6 @@ const arg1 = (process.argv[2]);
 const arg2 = (process.argv[3]);
 
 // print using console log using format arg1 is arg2
-console.log(arg1 + "is" + arg2);
+console.log(arg1 + " is " + arg2);
 
 
