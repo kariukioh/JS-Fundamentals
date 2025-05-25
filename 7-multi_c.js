a0007d0a663d4ed8ec4c6f2b1dcccc6912ed06e6
kariukik <kariukioh@gmail.com>
2025-05-25 17:48:59 +0300

    printing a script x number of times

---
+++ 7-multi_c.js
@@ -0,0 +1,13 @@
+const x = process.arg[2]; //acessing the first argument
+const num = parseInt(x); //converting to a number
+
+if (isNaN(num)) {
+	console.log("Missing number of occurrences");
+} else {
+	let i = 0;
+	while (i < num) {
+		console.log("C is fun");
+		i++;
+	}
+}
+
